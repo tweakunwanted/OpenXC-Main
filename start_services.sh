739b49d1fcb37eb6a19df5ac0c9c32ee4e822935
tweakunwanted <tweakunwanted@users.noreply.github.com>
2019-10-08 19:38:39 -0300

    decoded files

--- start_services.sh
+++ start_services.sh
@@ -8,7 +8,6 @@ sleep 4
 sudo -u xtreamcodes /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/setup_cache.php
 sudo -u xtreamcodes /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/tools/signal_receiver.php >/dev/null 2>/dev/null &
 sudo -u xtreamcodes /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/tools/pipe_reader.php >/dev/null 2>/dev/null &
-chown -R xtreamcodes:xtreamcodes /sys/class/net
 chown -R xtreamcodes:xtreamcodes /home/xtreamcodes
 sleep 4
 /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/sbin/nginx_rtmp
